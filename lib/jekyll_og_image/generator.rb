74592e8b1108a870d4585e039be3f969d0a6a4a1
Ben Balter <ben@balter.com>
2024-09-08 19:02:48 -0400

    add desc

--- lib/jekyll_og_image/generator.rb
+++ lib/jekyll_og_image/generator.rb
@@ -2,8 +2,12 @@
 
 class JekyllOgImage::Generator < Jekyll::Generator
   safe true
+  include Jekyll::Filters
+  include Liquid::StandardFilters
 
   def generate(site)
+    @context = Liquid::Context.new({}, {}, { site: site })
+
     base_path = File.join(JekyllOgImage.config.output_dir, "posts")
 
     FileUtils.mkdir_p File.join(site.config["source"], base_path)
@@ -20,11 +24,13 @@ class JekyllOgImage::Generator < Jekyll::Generator
         Jekyll.logger.info "Jekyll Og Image:", "Skipping image generation #{path} as it already exists." if JekyllOgImage.config.verbose?
       end
 
+      description = strip_html(markdownify(post.data["description"])).strip
+
       post.data["image"] ||= {
         "path" => File.join(base_path, "#{post.data['slug']}.png"),
         "width" => 1200,
         "height" => 600,
-        "alt" => post.data["title"]
+        "alt" => "#{post.data["title"]}: #{description}",
       }
     end
   end
@@ -38,8 +44,9 @@ class JekyllOgImage::Generator < Jekyll::Generator
     canvas = add_border_bottom(canvas, config) if config.border_bottom
     canvas = add_image(canvas, File.read(File.join(site.config["source"], config.image))) if config.image
     canvas = add_header(canvas, post, config)
-    canvas = add_publish_date(canvas, post, config)
-    canvas = add_tags(canvas, post, config) if post.data["tags"].any?
+    #canvas = add_publish_date(canvas, post, config)
+    #canvas = add_tags(canvas, post, config) if post.data["tags"].any?
+    convas = add_description(canvas, post, config) if post.data["description"]
     canvas = add_domain(canvas, post, config) if config.domain
 
     canvas.save(path)
@@ -99,6 +106,17 @@ class JekyllOgImage::Generator < Jekyll::Generator
       gravity: :sw,
       color: config.content.color,
       dpi: 150,
+      font: config.content.font_family
+    ) { |_canvas, _text| { x: 80, y: config.margin_bottom } }
+  end
+
+  def add_description(canvas, post, config)
+    description = strip_html(markdownify(post.data["description"])).strip
+    canvas.text(description,
+      width: (config.image ? 870 : 1040) * 0.8,
+      gravity: :sw,
+      color: config.content.color,
+      dpi: 175,
       font: config.content.font_family
     ) { |_canvas, _text| { x: 80, y: config.margin_bottom } }
   end
